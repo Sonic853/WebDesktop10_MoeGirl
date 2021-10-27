48d37aa22486689ead56cf38d1094b17c8da25f1
Sonic853 <sonic853@qq.com>
2021-10-27 19:08:56 +0800

    第一个版本

---
+++ script.js
@@ -0,0 +1,40 @@
+let windows = document.querySelectorAll("#Desktop10>.Window");
+let winSet = new class {
+    drag = false;
+    left = 0;
+    top = 0;
+    currentX = 0;
+    currentY = 0;
+    obj;
+    select = undefined;
+};
+windows.forEach(obj => {
+    const title = obj.querySelector(".Title");
+    title.onmousedown = e => {
+        if (e.button == 0) {
+            winSet.drag = true;
+            winSet.obj = obj;
+            winSet.left = obj.offsetLeft;
+            winSet.top = obj.offsetTop;
+            winSet.currentX = e.clientX;
+            winSet.currentY = e.clientY;
+            winSet.select = "Window";
+        }
+    };
+});
+document.addEventListener("mouseup", e => {
+    if (winSet.obj && winSet.select == "Window") {
+        if (winSet.obj.offsetTop < 0) {
+            winSet.obj.style.top = "0px";
+        }
+    }
+    winSet.drag = false;
+    winSet.obj = winSet.select = undefined;
+    winSet.left = winSet.top = winSet.currentX = winSet.currentY = 0;
+});
+document.addEventListener("mousemove", e => {
+    if (winSet.drag && winSet.obj && winSet.select == "Window") {
+        winSet.obj.style.left = (winSet.left + (e.clientX - winSet.currentX)) + "px";
+        winSet.obj.style.top = (winSet.top + (e.clientY - winSet.currentY)) + "px";
+    }
+});
